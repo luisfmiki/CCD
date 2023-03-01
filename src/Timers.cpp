829bfa27373e33afebc1f55a4284980f782ad1e3
luisfmiki <luisfmiki@hotmail.com>
2023-03-01 14:47:01 -0300

    1mar

---
+++ src/Timers.cpp
@@ -0,0 +1,35 @@
+#include "Timers.h"
+
+/* aqui usamos o timer1 (16-bit) para gerar o SI no pino 9 (OC1A) */ 
+
+void cfgTimerSI(int tINT){ 
+    TCCR1A = (1 << COM1A1); // Clear OC1A/OC1B on compare match when up-counting. Set  
+                            // OC1A/OC1B on compare match when down counting 
+    TCCR1B = (1 << WGM13); // modo 8 da tabela 15-5 
+
+    int TempoTotal = tINT + 3; // esse 3ms vem de dos 18 ciclos de clock que precedem o tempo de integracao  
+    ICR1 = uint16_t((TempoTotal / 0.008) + 1); // definindo o valor de topo com base em tINT 
+
+    TCCR1B &= ~(1 << CS12); // Set prescaler to 64 
+    TCCR1B |= (1 << CS11); 
+    TCCR1B |= (1 << CS10); 
+
+    OCR1A = 1; // periodo de high de 8us 
+
+    TIMSK1 = (1 << TOIE1); // habilita a interrup p/ ativar CLK 
+} 
+
+/* aqui usamos o timer0 para gerar o CLK no pino 6 (OC0B)*/ 
+
+void cfgTimerCLK(){ 
+    TCCR0A = (1 << COM2A0) | (1 << COM2B0) | (1 << WGM01); // modo CTC toggle 
+
+    TCCR0B = 0; // desativa o timer 
+
+    OCR0B = 3; // 128 -> 7.375kHz? <- ajustar esse valor para calibrar a freq de CLK 
+    OCR0A = 3; // idem, valor de comparacao de 8-bit 
+    
+    TCNT0 = 2; // reset timer0 
+
+    TIMSK0 = (1 << OCIE0A); 
+} 
--- src/main.cpp
+++ src/main.cpp
@@ -9,80 +9,28 @@
 uint16_t amostras[SIZE];
 volatile uint16_t amostraIndex = 0;
 
-/* varaiveis de controle das formas de onda */
-bool pulsoSI = false;
-bool risingCLK = false;
-
-
 /* bloco de interrupcoes ------------------------------------------- */
-ISR(TIMER2_COMPB_vect){
-    risingCLK = !risingCLK;
-    TCNT2 = 0;
-}
-ISR(TIMER1_COMPA_vect){
-    if(pulsoSI) {
-        OCR1A = 6250;   // 100ms, duracao maxima entre cada pulso SI
-        pulsoSI = false;
-        TCNT1 = 0;
-    } else {
-        OCR1A = 9;      // 144us, duracao do pulso SI no caso de CLK de 7375Hz
-        pulsoSI = true;
-        TCNT1 = 0;
-    }
-}
-ISR(ADC_vect){
-    if (risingCLK)
-    {
-        amostras[amostraIndex] = ADC;
-        amostraIndex++;
-        ADCSRA &= ~_BV(ADIE);
+ISR(TIMER1_OVF_vect){ 
+TCCR0B = 0x2; // calibra o prescale (ativa o timer0 -> CLK) 
+} 
 
-        /* desativa os timers */
-        if (amostraIndex > 127) {
-            TIMSK1 = 0;
-            TIMSK2 = 0;
-            TCCR1B = 0;
-            TCCR2B = 0;
-        }
-    }
-}
-/* fim do bloco de interrupcoes ------------------------------------------- */
+ISR(ADC_vect){ 
+    amostras[amostraIndex] = ADC; 
+    amostraIndex++; 
+    if (amostraIndex > 127) {  
+        TCCR0B = 0; // desabilida o timer0 (CLK) 
+        amostraIndex = 0; 
+    } 
+} 
 
+void setup(){ 
+    TCCR0B = 0; // desativa o timer 
+    pinMode(9, OUTPUT); 
+    pinMode(6, OUTPUT); 
 
+    cfgADC(); 
+    cfgTimerCLK(); 
+    cfgTimerSI(50); 
+} 
 
-void setup()
-{
-    TIMSK0 = 0; // desativa ints do timer0
-
-    int tINT;
-    Serial.begin(115200);
-    Serial.print("Entre com o tempo de integracao desejado em ms(11ms < t < 100ms): ");
-    tINT = Serial.read();
-    while (tINT < 11 || tINT > 100)
-    {
-        Serial.println("Valor invalido");
-        Serial.print("Entre com o tempo de integracao desejado em ms(11ms < t < 100ms): ");
-        tINT = Serial.read();
-    }
-
-    cli();
-    cfgADC();
-    cfgTimerCLK();
-    cfgTimerSI(0);
-    sei();
-
-}
-
-void loop()
-{
-    
-
-
-
-    // while (ADCSRA & _BV(ADIE))
-    //     ; // Wait for audio sampling to finish
-    // cli();
-    
-    //     ADCSRA |= _BV(ADIE); // Resume sampling interrupt
-    //     sei();
-}
+void loop(){} 
